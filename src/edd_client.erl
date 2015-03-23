5429bde931e8269342e9347a462e477eaa37749f
Salvador Tamarit <tamarit27@gmail.com>
2015-03-23 16:27:01 +0100

    Changes on server and client
    - Improved client interface
    - Server serving JSON

--- src/edd_client.erl
+++ src/edd_client.erl
@@ -1,23 +1,20 @@
 -module(edd_client).
 
--export([client/1, start/0]).
+-export([client/3, start/2]).
 
-% -define(CLIENT_DELAY,1500).
 -define(DEF_PORT, 		6667).
 
-start()->
-	client(?DEF_PORT).
+start(Dir, Call) ->
+	client(?DEF_PORT, Dir, Call).
 
 
-client(Port) ->
+client(Port, Dir, Call) ->
 	{ok, Sock} = gen_tcp:connect("localhost", Port, 
                                  [binary, {packet, 0}, {active, false}]),
-	client_control(Sock).
-	% spawn(fun() -> client_receive(Sock) end),
-	% spawn(fun() -> client_loop(Sock,0) end).
+	client_control(Sock, Dir, Call).
 
-client_control(Sock) ->
-    case gen_tcp:send(Sock,"/Users/tama/Dropbox/faena/current work/edd/examples/queue/") of 
+client_control(Sock, Dir, Call) ->
+    case gen_tcp:send(Sock,Dir) of 
     	ok -> 
 			ok;
 		{error,Reason} ->
@@ -25,7 +22,7 @@ client_control(Sock) ->
 			erlang:error(error)
     end,
 	client_receive(Sock),
-    case gen_tcp:send(Sock,"queue_old:out_r({[1],lists:seq(1,10)})") of 
+    case gen_tcp:send(Sock,Call) of 
     	ok -> 
 			ok;
 		{error,Reason2} ->
@@ -37,14 +34,8 @@ client_control(Sock) ->
 client_receive(Sock) ->
 	case gen_tcp:recv(Sock, 0) of
         {ok, Data} ->
-            io:format("~s", [binary_to_list(Data)]);
+            io:format("Answer: ~s\n", [binary_to_list(Data)]);
         {error, Reason} ->
             io:format("Error: ~p\n",[Reason]),
             erlang:error(error)
     end.
-
-
-% %% @doc Format a list of string to be sent to a client
-% -spec pack([string()]) -> string().
-% pack(List) ->
-% 	string:join(List, ":") ++ "\n".
--- src/edd_server.erl
+++ src/edd_server.erl
@@ -15,37 +15,27 @@
 
 %%%_* application callbacks ----------------------------------------------------
 
-%% @doc Start a chat server API in port 
+
 -spec start() -> ok.
 start() ->
 	server(?DEF_PORT).
 
 %%%_* External functions -------------------------------------------------------
 
-%% @doc Chat server control entry point
+
 -spec server_entry_point(port()) -> ok.
 server_entry_point(Socket) ->
-	% {ok, Name, Users} = 
-	% 	gen_server:call(chat_control, {connect, Socket}),
-	% gen_server:cast(chat_control, 
-	% 	{individual, Name, pack([?CONNECT, ?OK, Name])}),
-	% gen_server:cast(chat_control, 
-	% 	{individual, Name, pack([?USERSLIST, Users])}),
-	% gen_server:cast(chat_control, 
-	% 	{broadcast, Name, pack([?JOIN, Name])}),
     server_listener(Socket).
 
 %%%_* Internal functions -------------------------------------------------------
 
-%% @doc Start a chat server API in a given port
 -spec server(integer()) -> ok.
 server(Port) ->
 	edd_control:start(),
     edd_tcp:start(?MODULE, Port, fun server_entry_point/1),
     ok.
 
-%% @doc Server loop listing in the socket corresponding to a user
-% -spec server_listener(port(), string()) -> ok.
+
 server_listener(Socket) ->
 	Dir = 
 	    case gen_tcp:recv(Socket, 0) of
@@ -73,88 +63,15 @@ server_listener(Socket) ->
 	        {error, closed} ->
 	            disconnect(Socket)
 	    end,
-    case gen_tcp:send(Socket, "ok") of
+	G = edd:dd_server(BuggyCall,Dir),
+	JSON_G = edd_lib:json_graph(G),
+    case gen_tcp:send(Socket, JSON_G) of
         ok -> 
             ok;
         {error,closed} ->
             disconnect(Socket)
-    end,
-    Return = edd:dd_server(BuggyCall,Dir),
-    io:format("Return: ~s\n",[edd_lib:json_graph(Return)]).
+    end.
 
-
-% %% @doc Broadcast a message in the chat
-% -spec say(port(), string(), string()) -> ok.
-% say(Socket, Name, Content) ->
-%     gen_server:cast(chat_control, 
-%     	{broadcast, Name, pack([?SAID, Name, Content])}),
-%     server_listener(Socket, Name).
-
-% %% @doc Disconnect a user from the chat
-% -spec disconnect(port(), string()) -> ok.
 disconnect(_Socket) ->
-	% gen_server:call(chat_control, {disconnect, Name}),
-	% gen_server:cast(chat_control, 
-	% 	{individual, Name, pack([?CLOSE])}),
-	% gen_server:cast(chat_control, 
-	% 	{broadcast, Name, pack([?LEFT, Name])}), 
     ok.
 
-% %% @doc Changes the name to a user
-% -spec change_name(port(), string(), string()) -> ok.
-% change_name(Socket, CurrentName, NewName) ->
-% 	FinalName = 
-% 		case string:tokens(NewName, ":") of 
-% 			[_] ->  
-% 				case gen_server:call(chat_control, 
-% 						{change_name, CurrentName, NewName}) of 
-% 					ok ->
-% 						gen_server:cast(chat_control, 
-% 							{individual, NewName, 
-% 								pack([?NAME_CHANGE, ?OK])}), 
-% 						gen_server:cast(chat_control, 
-% 							{broadcast, NewName, 
-% 								pack([?NAME_CHANGE, CurrentName, NewName])}),
-% 						NewName;
-% 					{error,used_name} ->
-% 						gen_server:cast(chat_control, 
-% 							{individual, CurrentName, 
-% 								pack([?NAME_CHANGE, ?ERROR,?E_IN_USE])}), 
-% 						CurrentName
-% 				end;
-% 			% The name contains character $:, so it's not valid
-% 			_ -> 
-% 				gen_server:cast(chat_control, 
-% 					{individual, CurrentName, 
-% 						pack([?NAME_CHANGE, ?ERROR,?E_NOT_VALID])}), 
-% 				CurrentName
-% 		end,
-% 	server_listener(Socket, FinalName).
-
-% %% @doc Sends a private message to a user
-% -spec private_message(port(), string(), string(), string()) -> ok.
-% private_message(Socket, Name, To, Msg) ->
-% 	case gen_server:call(chat_control, {private_message, Name, To}) of 
-% 		ok ->
-% 			gen_server:cast(chat_control, 
-% 				{individual, Name, pack([?PRIVATE, ?OK])}),
-% 			gen_server:cast(chat_control, 
-% 				{individual, To, pack([?PRIVATE, ?SAID, Name, Msg])});
-% 		{error,same_user} ->
-% 			gen_server:cast(chat_control, 
-% 				{individual, Name, pack([?PRIVATE, ?ERROR, ?E_SAME_USER])});
-% 		{error,user_not_exists} ->
-% 			gen_server:cast(chat_control, 
-% 				{individual, Name, pack([?PRIVATE, ?ERROR, ?E_NOT_EXISTS])})
-% 	end,
-% 	server_listener(Socket, Name).
-
-% %% @doc Format a list of string to be sent to a client
-% -spec pack([string()]) -> string().
-% pack(List) ->
-% 	string:join(List, ":") ++ "\n".
-
-% %% @doc Remove the carriage return from the messages
-% -spec remove_cr(string()) -> string().
-% remove_cr(String) ->
-% 	string:strip(String, right, $\n).
