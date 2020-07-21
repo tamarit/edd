6973115e9730c610cb15ede7a042fdaaca85471b
Salvador Tamarit <stamarit@software.imdea.org>
2020-07-21 04:37:38 +0200

    Minor

--- src/edd_test_writer.erl
+++ src/edd_test_writer.erl
@@ -4,11 +4,26 @@
 
 write(G, NewCorrect, NewNotCorrect, OtherTests) ->
 	% io:format("~p\n", [{NewCorrect, NewNotCorrect}]),
+	% [begin
+	% 	io:format("~p\n", [V]),
+	% 	{ok,Toks,_} = erl_scan:string(V ++ "."),
+	% 	{ok,[Aexpr|_]} = erl_parse:parse_exprs(Toks),
+	% 	case Aexpr of 
+	% 		{call,_,{remote,_,{atom,_,_},_},APars} ->
+	% 			io:format("~p\n", [APars]) ;
+	% 		_ ->
+	% 			[]
+	% 	end	
+	% end 
+	% 	|| V <- NewCorrect],
+	% [io:format("~p\n", [edd_lib:get_MFA_Label(G,V)]) 
+	% 		|| V <- NewNotCorrect],
 	LabeledVertex = 
 			[{V, element(1, edd_lib:get_MFA_Label(G,V)), equal} 
 			 || V <- NewCorrect] 
 		++ 	[{V, element(1, edd_lib:get_MFA_Label(G,V)), not_equal} 
 		 	 || V <- NewNotCorrect],
+	% io:format("~p\n", [LabeledVertex]),
 	LabeledOtherTests = 
 		lists:flatten(
 			[begin
@@ -86,8 +101,34 @@ write_in_file(Module, Tests, G) ->
 	StrTests1 = 
 		[ build_test_case(Type, Call, Value) 
 		 || {Call, Value, Type} <- PreviousTests],
+	put(complexity_total, []),
+	io:format("******* COMPLEXITY DETAILS *******\n", []),  
 	StrTests2 = 
 		build_tests(Tests, G, []),
+	io:format("**********************************\n", []), 
+	io:format("******* COMPLEXITY SUMMARY *******\n", []),  
+	% io:format("~p\n", [get(complexity_total)]),
+	case sets:is_element(unknown, sets:from_list(get(complexity_total))) of
+		true -> 
+			io:format("WARNING: Some complexity are unknown");
+		false -> 
+			ok 
+	end,
+	WithoutUnknowns = [ComFun || ComFun <- get(complexity_total), ComFun =/= unknown ],
+	SumComplexities = lists:sum(WithoutUnknowns),
+	case SumComplexities of 
+		0 -> 
+			io:format("Total complexity unknown\n", []);
+		_ ->
+			MaxComplexities = lists:max(WithoutUnknowns),
+			MinComplexities = lists:min(WithoutUnknowns),
+			MeanComplexity = SumComplexities/length(WithoutUnknowns),
+			io:format("Total complexity: ~p\n", [SumComplexities]),
+			io:format("Max complexity: ~p\n", [MaxComplexities]),
+			io:format("Min complexity: ~p\n", [MinComplexities]),
+			io:format("Average complexity: ~p\n", [MeanComplexity])
+	end,
+	io:format("**********************************\n", []), 
 	StrTests = 
 		StrTests1 ++ StrTests2,
 	StrTestFun = 
@@ -194,21 +235,63 @@ str_assert(not_equal) ->
 	"?assertNotEqual".
 
 build_test_case(Type, StrCall0, StrValue) ->
+	% io:format("Question: " ++ StrCall0 ++ "\n"),
 	{ok,Toks,_} = erl_scan:string(lists:flatten(StrCall0)++"."),
 	{ok,[Aexpr|_]} = erl_parse:parse_exprs(Toks),
 	NAexp = disremotize(Aexpr),
 	StrCall = erl_prettypr:format(NAexp),
+	io:format(
+		"Question:\n\t~s = ~s\n", 
+		[StrCall, StrValue]),
+	try
+		{ok,ToksValue,_} = erl_scan:string(lists:flatten(StrValue)++"."),
+		{ok,[AexprValue|_]} = erl_parse:parse_exprs(ToksValue),
+		% io:format("v: ~p\n", [AexprValue]),
+		{value, ValueTerm, []} = erl_eval:expr(AexprValue, []),
+		ComplexityValue = edd_con:complexity_term(ValueTerm),
+		% io:format("ComplexityValue: ~p\n", [ComplexityValue]),
+		ComplexityCall = get(complexity),
+		% io:format("ComplexityCall: ~p\n", [ComplexityCall]),
+		ComplexityQuestion = 1 + ComplexityCall + ComplexityValue,
+		io:format("Complexity:\n\t~p\n", [ComplexityQuestion]),
+		put(complexity_total, [ComplexityQuestion | get(complexity_total)])
+	catch
+		_:_ ->
+			put(complexity_total, [unknown | get(complexity_total)]),
+			io:format("Complexity:\n\t~p\n", [unknown])
+	end,
 	lists:flatten(
 		io_lib:format(
 			"~s(~s, ~s)", 
 			[str_assert(Type), StrCall, StrValue])).
-
+	
 
 disremotize(Node) ->
 	erl_syntax_lib:map(fun disremotize_node/1, Node).
 
 disremotize_node(Node) -> 
 	try
+		% io:format("~p\n", [erl_syntax:application_arguments(Node)]),
+		% io:format("~p\n", [erl_syntax:concrete(lists:nth(1, erl_syntax:application_arguments(Node)))]),
+		% io:format("~p\n", [edd_con:complexity_term(erl_syntax:concrete(lists:nth(1, erl_syntax:application_arguments(Node))))]),
+		try
+			Complexity = 
+				lists:sum(
+					lists:map(
+						fun(AppArg) -> 
+							edd_con:complexity_term(
+								erl_syntax:concrete(AppArg)
+							) 
+						end, 
+						erl_syntax:application_arguments(Node)
+					)
+				),
+			% io:format("ComplexityCall: ~p\n", [Complexity]),
+			put(complexity, Complexity)
+		catch
+			_:_ ->
+				put(complexity, unknown)
+		end,
 		Operator = erl_syntax:application_operator(Node),
 		Module = get(module),
 		Module = 
@@ -216,9 +299,10 @@ disremotize_node(Node) ->
 				erl_syntax:module_qualifier_argument(Operator)),
 		erl_syntax:application(
 			erl_syntax:module_qualifier_body(Operator), 
-			erl_syntax:application_arguments(Node)) 
+			erl_syntax:application_arguments(Node))
 	catch 
 		_:_ ->
+			put(complexity, unknown),
 			disremotize_implicit_fun(Node)
 	end.
 
@@ -235,7 +319,7 @@ disremotize_implicit_fun(Node) ->
 			erl_syntax:atom_value(
 				erl_syntax:module_qualifier_argument(Name)),
 		erl_syntax:implicit_fun(
-			erl_syntax:module_qualifier_body(Name))
+			erl_syntax:module_qualifier_body(Name)),
 	catch 
 		_:_ ->
 			Node
